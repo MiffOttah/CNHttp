9d42434952535734543415cebf98d666045224a3
Miff <miff@miffthefox.info>
2020-05-06 16:22:27 -0400

    Rewrite redirection tests to match newer WebRequest API that throws an exception on a redirect

--- CNHttp.Test/WebRequestFixtures/Redirect.cs
+++ CNHttp.Test/WebRequestFixtures/Redirect.cs
@@ -14,81 +14,72 @@ namespace NHttp.Test.WebRequestFixtures
         [Test]
         public void RelativeRedirect()
         {
-            using (var server = new HttpServer())
+            using var server = new HttpServer();
+            server.RequestReceived += (s, e) => e.Response.Redirect("target");
+            server.Start();
+
+            var request = (HttpWebRequest)WebRequest.Create($"http://{server.EndPoint}/source/page");
+            request.AllowAutoRedirect = false;
+
+            try
             {
-                server.RequestReceived += (s, e) => e.Response.Redirect("target");
-
-                server.Start();
-
-                var request = (HttpWebRequest)WebRequest.Create(
-                    String.Format("http://{0}/source/page", server.EndPoint)
-                );
-
-                request.AllowAutoRedirect = false;
-
-                using (var response = (HttpWebResponse)request.GetResponse())
-                {
-                    string redirectLocation = String.Format(
-                        "http://{0}/source/target", server.EndPoint
-                    );
-
-                    Assert.AreEqual(HttpStatusCode.Found, response.StatusCode);
-                    Assert.AreEqual(redirectLocation, response.Headers["Location"]);
-                }
+                request.GetResponse();
+                Assert.Fail();
+            }
+            catch (WebException ex)
+            {
+                var response = (HttpWebResponse)ex.Response;
+                string redirectLocation = $"http://{server.EndPoint}/source/target";
+                Assert.AreEqual(HttpStatusCode.Found, response.StatusCode);
+                Assert.AreEqual(redirectLocation, response.Headers["Location"]);
             }
         }
 
         [Test]
         public void AbsoluteRedirect()
         {
-            using (var server = new HttpServer())
+            using var server = new HttpServer();
+            server.RequestReceived += (s, e) => e.Response.Redirect("/target");
+            server.Start();
+
+            var request = (HttpWebRequest)WebRequest.Create($"http://{server.EndPoint}/source/page");
+            request.AllowAutoRedirect = false;
+
+            try
             {
-                server.RequestReceived += (s, e) => e.Response.Redirect("/target");
-
-                server.Start();
-
-                var request = (HttpWebRequest)WebRequest.Create(
-                    String.Format("http://{0}/source/page", server.EndPoint)
-                );
-
-                request.AllowAutoRedirect = false;
-
-                using (var response = (HttpWebResponse)request.GetResponse())
-                {
-                    string redirectLocation = String.Format(
-                        "http://{0}/target", server.EndPoint
-                    );
-
-                    Assert.AreEqual(HttpStatusCode.Found, response.StatusCode);
-                    Assert.AreEqual(redirectLocation, response.Headers["Location"]);
-                }
+                request.GetResponse();
+                Assert.Fail();
+            }
+            catch (WebException ex)
+            {
+                var response = (HttpWebResponse)ex.Response;
+                string redirectLocation = $"http://{server.EndPoint}/target";
+                Assert.AreEqual(HttpStatusCode.Found, response.StatusCode);
+                Assert.AreEqual(redirectLocation, response.Headers["Location"]);
             }
         }
 
         [Test]
         public void PermanentRedirect()
         {
-            using (var server = new HttpServer())
+            using var server = new HttpServer();
+            server.RequestReceived += (s, e) => e.Response.RedirectPermanent("/target");
+            server.Start();
+
+            var request = (HttpWebRequest)WebRequest.Create($"http://{server.EndPoint}/source/page");
+            request.AllowAutoRedirect = false;
+
+            try
             {
-                server.RequestReceived += (s, e) => e.Response.RedirectPermanent("target");
-
-                server.Start();
-
-                var request = (HttpWebRequest)WebRequest.Create(
-                    String.Format("http://{0}/source/page", server.EndPoint)
-                );
-
-                request.AllowAutoRedirect = false;
-
-                using (var response = (HttpWebResponse)request.GetResponse())
-                {
-                    string redirectLocation = String.Format(
-                        "http://{0}/source/target", server.EndPoint
-                    );
-
-                    Assert.AreEqual(HttpStatusCode.MovedPermanently, response.StatusCode);
-                    Assert.AreEqual(redirectLocation, response.Headers["Location"]);
-                }
+                request.GetResponse();
+                Assert.Fail();
+            }
+            catch (WebException ex)
+            {
+                var response = (HttpWebResponse)ex.Response;
+                string redirectLocation = $"http://{server.EndPoint}/target";
+                Assert.AreEqual(HttpStatusCode.MovedPermanently, response.StatusCode);
+                Assert.AreEqual(redirectLocation, response.Headers["Location"]);
             }
         }
     }
